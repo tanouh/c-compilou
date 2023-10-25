be0864bdbbaf0154bf5e541bbf3944abb95b02a1
Frédéric Srichanwit <frederic.srichanwit@etu.sorbonne-universite.fr>
2023-10-25 10:42:12 +0200

    aaaaah

--- ast_mips.ml
+++ ast_mips.ml
@@ -5,13 +5,11 @@ and i_AST =
 | Iif of i_expr*i_AST*i_AST | Iblock of i_AST list
 | Ireturn of i_expr | Iassign of i_left_value*i_expr
 | Ival of i_expr
-and i_value =
-| Ileft of i_left_value | Iconst of int
 and i_left_value = i_pos * int (* position in memory and size *)
 and i_pos =
 | Ilocal of int (* offset to FP *)
 | Iglobal of string (* label *)
 | Ideref of i_expr (* for pointers *)
 and i_expr =
-| Iunop of i_value | Ibinop of binop * i_value * i_value
+| Iunop of i_expr | Ibinop of binop * i_expr * i_expr
 | Icall of i_pos * string * int * i_expr list(* label + offset *) | Ileft of i_left_value | Iconst of int
