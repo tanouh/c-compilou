22db558150f3d3813a9a7f1010a902f79f93366f
garnierk <Kevin_Garnier@outlook.fr>
2023-10-31 18:21:27 +0100

    update readme

--- test.c
+++ test.c
@@ -1,8 +1,11 @@
-void print(int a, int b, int c, int d, int e, int f) {
-    print_int(a,b,c,d,e,f);
-}
+void print(int a);
 
 int main() {
-    /* int x = 5 + print(1,2,3,4,5,6);
-    print_int(1);
+
+    /* int x = 5 + print(1,2,3,4,5,6); */
+    print(1);
+}
+
+void print(int a) {
+    print_int(a);
 }
