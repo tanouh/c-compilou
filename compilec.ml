6277d4c6e6c4a94c7d6f1397826665caa74cb9bb
Alexis Dobrzynski <alexisdobrzynski@gmail.com>
2023-10-31 03:15:39 +0000

    Update file compilec.ml

--- compilec.ml
+++ compilec.ml
@@ -35,6 +35,8 @@ let convert_cond (o:binop) = match o with
 let int_of_bool b = if b then 1 else 0
 let bool_of_int i = if i=0 then false else true
 
+(* simplifie les expressions, en remplçant 4+3 par 7 mais en laissant 4 + f(3), sans calculer f(3) 
+   renvoie des iexprs *)
 let rec eval_expr hashtable_loc e =
   match e with
   | Const Int c -> Iconst(int_of_string c)
@@ -56,33 +58,35 @@ let rec eval_expr hashtable_loc e =
     (match op with
     | Add | Sub | Mul | Div | Mod -> (match (eval_expr hashtable_loc e1, eval_expr hashtable_loc e2) with
       |(Iconst i1 , Iconst i2) ->  Iconst (convert_arith op i1 i2)
-      |(ie1,ie2) -> Ibinop (op,ie1,ie2))
+      |(ie1,ie2) -> Ibinop (op,ie1,ie2)) (* on ne fait rien car ce ne sont pas directement deux constantes *)
     | Leq | Le | Geq | Ge | Neq | Eq -> (match (eval_expr hashtable_loc e1, eval_expr hashtable_loc e2) with
       |(Iconst i1 , Iconst i2) ->  Iconst (int_of_bool (convert_comp op i1 i2))
-      |(ie1,ie2) -> Ibinop (op,ie1,ie2))
+      |(ie1,ie2) -> Ibinop (op,ie1,ie2)) (* idem *)
     | And | Or -> (match (eval_expr hashtable_loc e1, eval_expr hashtable_loc e2) with
       |(Iconst i1 , Iconst i2) ->  Iconst (int_of_bool (convert_cond op (bool_of_int i1) (bool_of_int i2)))
-      |(ie1,ie2) -> Ibinop (op,ie1,ie2))
+      |(ie1,ie2) -> Ibinop (op,ie1,ie2)) (* idem *)
   )
-  | Ecall (name,expl) -> check_call hashtable_loc name expl
+  | Ecall (name,expl) -> check_call hashtable_loc name expl 
   | _ -> raise (Error_no_pos " Array à faire plus tard")
 
+  (* vérifie que la fonction est définie et que les arguments expl correspondent à ceux attendu par la fonction*)
   and check_call hashtable_loc name expl =
-    if Hashtbl.mem functions name then (
-      let args_compile =  List.map (eval_expr hashtable_loc) expl in (* on compile d'abord les arguments que prends la fonction name*)
-      let expr_to_type expr = match expr with 
+    if Hashtbl.mem functions name then ( (* la fonction name est déjà définie *)
+      let args_compile =  List.map (eval_expr hashtable_loc) expl in (* on compile d'abord les arguments que prend name*)
+      let expr_to_type expr = match expr with (* la fonction expr_to_type permettra de comparer les types des éléments de expl avec les types des arguments attendus par name *)
         | Icall( s , expr_l) -> fst (Hashtbl.find functions s )
-        | _ -> Dint 
+        | _ -> Dint (* une expression est forcément un int si ce n'est pas un ecall *)
        in
       match name with 
         | "print_int" -> if List.mem Dvoid (List.map expr_to_type args_compile) then raise (Error_no_pos ("wrong arguments for print_int")) 
-        else Icall(name,args_compile)
-        | _ -> 
-          if (List.map expr_to_type args_compile = snd (Hashtbl.find functions name) ) then Icall(name, args_compile)
+        else Icall(name,args_compile) (* la fonction print_int prend un nombre infini d'argument, il suffit donc de regarder si un argument n'est pas un int*)
+        | _ -> (* sinon il faut aussi que le nombre d'argument expl soit égale au nombre d'argument attendu par name *)
+          if (List.map expr_to_type args_compile = snd (Hashtbl.find functions name) ) (* compare taille et type *)
+            then Icall(name, args_compile)
           else
             raise (Error_no_pos (" wrong arguments for " ^ name))
     )
-    else raise (Error_no_pos ("Undefined function "^name)) (* TODO: check the type + check if funtion exist*)
+    else raise (Error_no_pos ("Undefined function "^name))
 
 let compile_if hashtable_loc cond body =
   let e = eval_expr hashtable_loc cond in
@@ -98,6 +102,7 @@ let compile_if_else hashtable_loc cond body_if body_else =
     | Icall(name,_) -> if fst (Hashtbl.find functions name) <> Dint then raise (Error_no_pos "if condition cannot be of type <void>") else Iifelse(e, body_if, body_else)
     | _ -> Iifelse(e, body_if, body_else)
 
+(* compile les statements grâce à eval_expr et renvoie des istatements *)
 let rec compile_stmt hashtable_loc (stmt,pos) = try (
   match stmt with
   | (Sassign (l,exp)) ->
@@ -126,16 +131,13 @@ let rec compile_stmt hashtable_loc (stmt,pos) = try (
 let compile_program p ofile =
   load_print_int;
   let aux x =
-    if Hashtbl.mem functions x.name then raise (Error_no_pos ("redeclaration of " ^ x.name ^ " with no linkage"))
-    else  Hashtbl.add functions x.name (x.ret_type, List.map fst x.args);
+    if Hashtbl.mem functions x.name then raise (Error_no_pos ("error: redefinition of " ^ x.name)) (* C ne permet pas de déclarer deux fonctions avec le même nom*)
+    else  Hashtbl.add functions x.name (x.ret_type, List.map fst x.args); (* ajoute la nouvelle variable gloable/fonction à la table*)
     match x.body with
-    | None -> (x.name, 0 ,List.length x.args,Iassign((Iglobal x.name, 4), IUndef))(* à modifier car comment gérer la déclaration de variable, convention à définir *)
-    | Some x_body -> let hashtable_loc = Hashtbl.create 5 in
-    List.iteri (fun i (t,name) -> Hashtbl.replace hashtable_loc name (i, Ileft (Ilocal i,4))) x.args; (*declare les arguments*)
+    | None -> (x.name, 0 ,List.length x.args, Iassign((Iglobal x.name, 4), IUndef)) (* c'est une variables globale *)
+    | Some x_body -> let hashtable_loc = Hashtbl.create 5 in (* créé la table des variables locales à la fonction x.name *)
+    List.iteri (fun i (t,name) -> Hashtbl.replace hashtable_loc name (i, Ileft (Ilocal i,4))) x.args; (*declare les arguments, permettant de gérer l'utilisation des arguments comme une variable locale à la fonction*)
     let body = compile_stmt hashtable_loc x_body in
       (x.name, Hashtbl.length hashtable_loc, List.length x.args, body) in
   let code = List.map aux p in
   to_mips (code,[]) ofile
-
-  (* let p = to_mips { text = code; data = [] } in
-  Mips.print_program p ofile *)
