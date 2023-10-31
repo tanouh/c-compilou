690252ade7c2c2312482647ab9fd10d70924d96a
Alexis Dobrzynski <alexisdobrzynski@gmail.com>
2023-10-31 00:07:18 +0000

    Update file compile_mips.ml

--- compile_mips.ml
+++ compile_mips.ml
@@ -1,186 +1,131 @@
-(* Production de code pour notre langage *)
-
-open Mips
+open Ast
 open Ast_mips
+open Compile_mips
 open Errors
 
-(* Les fonctions de manipulation de la pile pour rendre les instructions plus modulaires *)
-let push_var reg x nb = [ Arithi (Add, SP, SP, -4); Sw (reg, Areg (4, SP)) ]
+exception Error_no_pos of string
 
-(* Empiler une variable temporaire *)
-let push_tmp = [ Arithi (Add, SP, SP, -4); Sw (V 0, Areg (4, SP)) ]
+let functions = Hashtbl.create 10
 
-(* Dépiler *)
-let pop_tmp =
-  (* sp_aux := !sp_aux - 1; *)
-  [ Lw(V 0, Areg(4,SP)); Arithi (Add, SP, SP, 4) ]
+let load_print_int =
+  Hashtbl.replace functions "print_int"  ( Dvoid ; [] ) 
 
-(*allocate local vars*)
-let allocate_mem nb_vars = [ Arithi (Add, SP, SP, -4 * nb_vars) ]
+let string_of_binop = function
+  | Add -> " + "
+  | Sub -> " - "
+  | Mul -> " * "
+  | Div -> " / "
+  | Mod -> " % "
+  | Leq -> " <+ "
+  | Le -> " < "
+  | Geq -> " >= "
+  | Ge -> " > "
+  | Neq -> " != "
+  | Eq -> " = "
+  | And -> " && "
+  | Or -> " || "
 
-(* free local vars*)
-let free_mem nb_vars = [ Arithi (Add, SP, SP, 4 * nb_vars) ]
+let string_of_dtype = function
+  | Dint -> 
 
-(*save fp on stack*)
-let save_fp offset =
-  [ Sw (FP, Areg (4 * offset, SP)); Arithi (Add, FP, SP, 4 * offset) ]
+let convert_arith (o:binop) = match o with
+| Add -> (+)
+| Sub -> (-)
+| Mul -> ( * )
+| Div -> (/)
+| Mod -> (mod)
+| b -> raise (Error_no_pos "warning:" ^ string_of_binop b "is not an arithmetic operation")
 
-let restore_fp nb_vars = Lw (FP, Areg (4, FP)) :: free_mem (nb_vars + 1)
-let save_ra offset = [ Sw (RA, Areg (4 * offset, SP)) ]
+let convert_comp (o:binop) = match o with
+| Leq -> (<=)
+| Le -> (<)
+| Geq -> (>=)
+| Ge -> (>)
+| Neq -> (!=)
+| Eq -> (=)
+| b -> raise (Error_no_pos  "warning:" ^ string_of_binop b "is not a comparison operation")
 
-(* restore ra, restore fp, free local vars, jmp ra*)
-let end_of_fun nb_var =
-  (Lw (RA, Areg (4, FP)) :: Lw (FP, Areg (0, FP)) :: free_mem (nb_var + 2))
-  @ [ Jr RA ]
+let convert_cond (o:binop) = match o with
+| And -> (&&)
+| Or -> (||)
+| b -> raise (Error_no_pos  "warning:" ^ string_of_binop b "is not an conditional operation")
 
-(*puts label, save fp and ra, move sp position in fp, allocate memory for local vars *)
-let start_of_fun name nb_vars =
-  (Label name :: allocate_mem (nb_vars + 2))
-  @ save_ra (nb_vars + 1)
-  @ save_fp nb_vars
+let int_of_bool b = if b then 1 else 0
+let bool_of_int i = if i=0 then false else true
 
-(*retrive var postiion in stack*)
-let get_var_addr size offset = Areg (-size * (offset + 1), FP)
+let rec eval_expr hashtable_loc e =
+  match e with
+  | Const Int c -> Iconst(int_of_string c)
+  | Val v -> (match v with
+    | Var x -> (match Hashtbl.find_opt hashtable_loc x with
+      | None -> raise (Error_no_pos "Undefined value" ^ x)
+      | Some i -> i)
+  )
+  | Moins e ->
+    (match eval_expr hashtable_loc e with
+    | Iconst c -> Iconst (-1 * c)
+    | _ -> raise (Error_no_pos "Invalid expression"))
+  | Not n -> (
+    match eval_expr hashtable_loc n with
+    | Iconst c -> if c=0 then Iconst 1 else Iconst 0
+    | _ -> raise (Error_no_pos "Invalid expression")
+  )
+  | Op (op,e1,e2) ->
+    (match op with
+    | Add | Sub | Mul | Div | Mod -> (match (eval_expr hashtable_loc e1, eval_expr hashtable_loc e2) with
+      |(Iconst i1 , Iconst i2) ->  Iconst (convert_arith op i1 i2)
+      |(ie1,ie2) -> Ibinop (op,ie1,ie2))
+    | Leq | Le | Geq | Ge | Neq | Eq -> (match (eval_expr hashtable_loc e1, eval_expr hashtable_loc e2) with
+      |(Iconst i1 , Iconst i2) ->  Iconst (int_of_bool (convert_comp op i1 i2))
+      |(ie1,ie2) -> Ibinop (op,ie1,ie2))
+    | And | Or -> (match (eval_expr hashtable_loc e1, eval_expr hashtable_loc e2) with
+      |(Iconst i1 , Iconst i2) ->  Iconst (int_of_bool (convert_cond op (bool_of_int i1) (bool_of_int i2)))
+      |(ie1,ie2) -> Ibinop (op,ie1,ie2))
+  )
+  | Ecall (name,expl) -> check_call hashtable_loc name expl
+  | _ -> raise (Error_no_pos " Array à faire plus tard")
 
-let arith_of_binop = function
-  | Ast.Add -> Mips.Add
-  | Ast.Sub -> Mips.Sub
-  | Ast.Div -> Mips.Div
-  | Ast.Mul -> Mips.Mul
-  | _ -> failwith "non equivalent"
+  and check_call hashtable_loc name expl =
+    if Hashtbl.mem functions name then (
+      let args_compile =  List.map (eval_expr hashtable_loc) expl in
+      if (List.map (fun x -> match x with 
+          | Icall( s , expr_l) -> let ( ret_type ,args_ls) = Hashtbl.find s in 
+            ret_type 
+          | _ -> Dint ) args_compile = (fst (Hashtbl.find name ) )) then 
+        Icall(name, args_compile)
+      else 
+        raise (Error_no_pos (" wrong arguments for " ^ name))
+    )
+    else raise (Error_no_pos ("Undefined function "^name)) (* TODO: check the type + check if funtion exist*)
 
-let rec compile_i_ast = function
-  | Iif (e, a, b) -> []
-  | Iblock a -> List.map compile_i_ast a |> List.concat
-  | Ireturn e -> compile_i_expr e
-  | Iassign (l, e) -> compile_i_assign (l, e)
-  | Ival e -> compile_i_expr e
-  | No_op -> []
-
-and compile_i_left_value (ipos, size) =
-  match ipos with
-  | Ilocal offset -> [ Lw (V 0, get_var_addr size offset) ]
-  | Iglobal label -> [ Lw (V 0, Alab label) ]
-  | Ideref p -> []
-
-and compile_i_const_value k = [ Li (V 0, k) ]
-and compile_i_unop v = compile_i_expr v @ [ Arithi (Mul, V 0, V 0, -1) ]
-
-and compile_i_op (op : Ast.binop) =
-  match op with
-  | Add | Sub | Mul | Div -> [ Arith (arith_of_binop op, V 0, V 1, V 0) ]
-  | Mod -> [ Arith_div (V 0, V 1); Mfhi (V 0) ]
-  | Leq -> [ Slt (V 0, V 0, V 1); Xori (V 0, V 0, 1) ] (* a <= b = ! b < a*)
-  | Le -> [ Slt (V 0, V 1, V 0) ]
-  | Geq -> [ Slt (V 0, V 0, V 1); Xori (V 0, V 0, 1) ]
-  | Ge -> [ Slt (V 0, V 0, V 1) ]
-  | Neq -> [ Sltu (V 0, V 0, 1) ]
-  | Eq -> [ Xor (V 0, V 1, V 0); Sltu (V 0, V 0, 1) ]
-  | And -> [ And (V 0, V 1, V 0) ]
-  | Or -> [ Or (V 0, V 1, V 0) ]
-
-and compile_i_binop op a b =
-  match b with
-  | Iconst k -> compile_i_expr a @ [ Li (V 1, k) ] @ compile_i_op op
-  | _ ->
-      compile_i_expr a @ push_tmp @ compile_i_expr b
-      @ [ Lw (V 1, Areg (4, SP)) ]
-      @ compile_i_op op @ [Arithi (Add, SP, SP, 4)]
-
-and compile_i_assign (l, e) =
-  let tmp = compile_i_expr e and ipos, size = l in
-  match ipos with
-  | Ilocal offset -> tmp @ [ Sw (V 0, get_var_addr size offset) ]
-  | Iglobal label -> tmp @ [ Sw (V 0, Alab label) ]
-  | Ideref p -> tmp @ []
-
-and compile_i_args =
-  (*compile les ai*)
-  let rec loop_ai i = function
-    | e :: [] -> compile_i_expr e @ [ Move (A i, V 0) ]
-    | e :: l' ->
-        (*stocke temporairement la valeur le temps de caculer le reste*)
-        compile_i_expr e @ push_tmp
-        @ loop_ai (i + 1) l'
-        (*recupere le resultat temporaire qui sera dans a_i*)
-        @ pop_tmp
-        @ [ Move (A i, V 0) ]
-    | [] -> []
-  and loop_spi res = function
-    | [] -> res
-    | e :: l' -> loop_spi (res @ compile_i_expr e @ push_tmp) l'
-  in
-  function
-  | a0 :: a1 :: a2 :: a3 :: l' ->
-      (* on fait d'abord la loop sur les arguments qui iront sur la pile puis ceux qui iront dans les a_i*)
-      loop_spi [] l' @ loop_ai 0 (a0 :: a1 :: a2 :: [ a3 ])
-  | args -> loop_ai 0 args
-
-and compile_i_expr = function
-  | Iconst k -> compile_i_const_value k
-  | Ileft lv -> compile_i_left_value lv
-  | Iunop v -> compile_i_unop v
-  | Ibinop (op, a, b) -> compile_i_binop op a b
-  | IUndef -> raise (Error "Declared variable but not initialized")
-  | Icall (label, args) -> (
-      match label with
-      | "print_int" -> i_print_int [] args
-      | label -> compile_i_args args @ [ Jal label ])
-
-and i_print_int res = function
-  | [] -> res@[ Li (V 0, 11); Li (A 0, 10); Syscall ]
-  | e::[] -> (res @ compile_i_expr e
-  @ [
-      Move (A 0, V 0);
-      Li (V 0, 1);
-      Syscall;
-      Li (V 0, 11); Li (A 0, 10); Syscall
-    ])
-  | e :: l' ->
-      i_print_int
-        (res @ compile_i_expr e
-        @ [
-            Move (A 0, V 0);
-            Li (V 0, 1);
-            Syscall;
-            Li (V 0, 11);
-            Li (A 0, 32);
-            Syscall;
-          ])
-        l'
-
-let allocate_args nb_args =
-  let rec loop_ai i stop=
-    if i >= stop then []
-    else [Sw (A i, get_var_addr 4 i )] @ loop_ai (i+1) stop
-  and loop_spi res i =
-    if i >= nb_args then res
-    else loop_spi (res@[Lw (T 0,  Areg (4 * (nb_args + 2 - i), FP)); Sw (T 0, get_var_addr 4 i )]) (i+1)
-  in
-  if nb_args <= 3 then
-    loop_ai 0 nb_args
-  else
-    loop_ai 0 4@loop_spi [] 4
-
-
-
-
-
-let compile_main name nb_vars body =
-  (Label name :: allocate_mem (nb_vars + 1))
-  @ save_fp nb_vars @ compile_i_ast body @ restore_fp nb_vars
-  @ [ End_of_program ]
-
-let compile_fun name nb_args nb_var body =
-  start_of_fun name nb_var @ allocate_args nb_args @compile_i_ast body @ end_of_fun nb_var
+let rec compile_stmt hashtable_loc (stmt,_pos) = try (
+  match stmt with
+  | (Sassign (l,exp), pos) ->
+    (match l with
+    | Var x -> if Hashtbl.mem hashtable_loc x then (let exp_eval = eval_expr hashtable_loc exp in Hashtbl.replace hashtable_loc x exp_eval ;
+    Iassign ((Ilocal 0,4) ,exp_eval)) else raise (Error_no_pos ("Undefined variable "^x))  (* Assignement de variable à définir*))
+  | (Sval e , pos) -> Ival (eval_expr hashtable_loc e)
+  | (Sreturn e , pos) ->  Ireturn (eval_expr hashtable_loc e)
+  | (Sblock b , pos) -> Iblock (List.map (compile_stmt hashtable_loc) (List.map (fun x -> (x,0)) b))
+  | (Sdeclarevar (typ,Var x) , pos) -> if typ = Dint then (Hashtbl.replace hashtable_loc x (IUndef) ; No_op )
+    else raise (Error_no_pos (x^" canno't be of type <void>")) (* Assignement de variable à définir*)
+  ) with 
+    | Error_no_pos s -> Error ( s, _pos)
+  (* | _ -> raise (Error "à faire") *)
 
 (* Compile le programme p et enregistre le code dans le fichier ofile *)
-let to_mips (p, data) ofile =
-  let aux (name, nb_var,nb_args, body) =
-    match name with
-    | "main" -> compile_main name nb_var body
-    | name -> compile_fun name nb_var nb_args body
-  in
-  let code = List.map aux p |> List.concat in
-  let p = { text = code; data = [] } in
-  Mips.print_program p ofile
+let compile_program p ofile =
+  load_print_int;
+  let aux x =
+    Hashtbl.replace functions x.name x;
+    match x.body with
+    | None -> (x.name, 0 ,List.length x.args,Iassign((Iglobal x.name, 4), IUndef))(* à modifier car comment gérer la déclaration de variable, convention à définir *)
+    | Some x_body -> let hashtable_loc = Hashtbl.create 5 in
+    List.iteri (fun i (t,name) -> Hashtbl.replace hashtable_loc name (Ileft (Ilocal i,4))) x.args; (*declare les arguments*)
+    let body = compile_stmt hashtable_loc (x_body,0) in
+      (x.name, Hashtbl.length hashtable_loc, List.length x.args,body) in
+  let code = List.map aux p in
+  to_mips (code,[]) ofile
+
+  (* let p = to_mips { text = code; data = [] } in
+  Mips.print_program p ofile *)
