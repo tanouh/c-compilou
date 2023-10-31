c53e810a3ea35f22ecd2ecefad5256cf81a0c854
Frédéric Srichanwit <frederic.srichanwit@etu.sorbonne-universite.fr>
2023-10-31 16:58:43 +0100

    new test

---
+++ tests/test_oldnew.c
@@ -0,0 +1,13 @@
+int main(){
+    print_int((3*2+1)*(5-3));
+    print_int((((0+1)/(0+1))*2)-((0+1)*1));
+    int x;
+    x = 4;
+    print_int(10+(5+3*(2+x)));
+    int y;
+    y = 3;
+    print_int((x+2)*(y+10-(5*x)));
+    x = 1;
+    print_int((x+2)*(y+10-(5*x)));
+    return 0;
+}
